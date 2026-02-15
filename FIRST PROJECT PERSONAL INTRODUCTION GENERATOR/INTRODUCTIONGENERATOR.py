cb190f2dd239b7d975bb305246ec1c4c3f70963f
Saqib Anwar <abrosaqi78@gmail.com>
2026-02-15 01:51:47 -0800

    Add files via upload

---
+++ FIRST PROJECT PERSONAL INTRODUCTION GENERATOR/INTRODUCTIONGENERATOR.py
@@ -0,0 +1,17 @@
+# PERSONAL INTROUCTION GENERATOR
+
+#CREATED BY SAQIB ANWAR
+
+name=input("Enter your name :")
+age=int(input("Enter your age :"))
+field=input("Enter your Field :")
+city=input("Enter your city :")
+
+print("\n--- Introduction Card---")
+print("Name:", name)
+print("Age:", age)
+print("Field:", field)
+print("City:", city)
+
+
+print("\nHello! My name is",name,"I am",age,"years old and I study ",field+".I live in ",city+".")
