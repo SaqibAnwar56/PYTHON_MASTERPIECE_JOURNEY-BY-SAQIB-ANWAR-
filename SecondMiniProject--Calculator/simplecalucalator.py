13d91fa6c1b82daf1ec2c26e08279f031682b9c2
Saqib Anwar <abrosaqi78@gmail.com>
2026-02-16 00:28:24 -0800

    Add files via upload

---
+++ SecondMiniProject--Calculator/simplecalucalator.py
@@ -0,0 +1,13 @@
+# SIMPLE CALCULATOR
+# Designed By Saqib Anwar
+
+print("Welcome to the Simple Calculator!\n")
+
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print("\n--- Results ---")
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
