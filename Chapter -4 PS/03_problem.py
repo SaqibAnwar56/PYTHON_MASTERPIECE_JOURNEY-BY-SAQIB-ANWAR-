9bc35baa8c59f09584955c70f707fa054c072f9a
Saqib Anwar <abrosaqi78@gmail.com>
2026-02-18 09:44:21 -0800

    Add files via upload

---
+++ Chapter -4 PS/03_problem.py
@@ -0,0 +1,3 @@
+# 3. Check that a tuple type cannot be changed in python. 
+a=(1,2,3)
+a[0]=44 # this shows error
---
+++ Chapter -4 PS/04_problem.py
@@ -0,0 +1,4 @@
+# 4. Write a program to sum a list with 4 numbers. 
+
+Sumofnumber=[80,100,90,90]
+print(sum(Sumofnumber))
