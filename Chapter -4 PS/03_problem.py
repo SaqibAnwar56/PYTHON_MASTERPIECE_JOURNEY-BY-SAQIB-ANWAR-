a8b23064cd6ad22fd8dab6ec7ae367071070a116
Saqib Anwar <abrosaqi78@gmail.com>
2026-02-18 09:44:44 -0800

    Delete Chapter -4 PS directory

--- Chapter -4 PS/03_problem.py
+++
@@ -1,3 +0,0 @@
-# 3. Check that a tuple type cannot be changed in python. 
-a=(1,2,3)
-a[0]=44 # this shows error
--- Chapter -4 PS/04_problem.py
+++
@@ -1,4 +0,0 @@
-# 4. Write a program to sum a list with 4 numbers. 
-
-Sumofnumber=[80,100,90,90]
-print(sum(Sumofnumber))
