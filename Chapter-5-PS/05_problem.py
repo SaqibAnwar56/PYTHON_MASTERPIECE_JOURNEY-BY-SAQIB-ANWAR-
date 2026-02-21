3c9309789cebf8c37c7f519944fe08f78007d6c0
Saqib Anwar <abrosaqi78@gmail.com>
2026-02-21 02:06:00 -0800

    Add files via upload

---
+++ Chapter-5-PS/05_problem.py
@@ -0,0 +1,7 @@
+'''set = {}
+What is the type of 's'?'''
+
+set={} # as we knows it empty that why belongs to dictionary 
+# s.set() this is for set
+
+print(type(set))
---
+++ Chapter-5-PS/06_problem.py
@@ -0,0 +1,23 @@
+"""Create an empty dictionary. Allow 4 friends to enter their favorite language as 
+value and use key as their names. Assume that the names are unique."""
+
+d={}
+name=input("Enter Friends name :")
+lang=input("Enter languange name :")
+
+d.update({name:lang})
+name=input("Enter Friends name :")
+lang=input("Enter languange name :")
+
+d.update({name:lang})
+
+name=input("Enter Friends name :")
+lang=input("Enter languange name :")
+
+d.update({name:lang})
+
+name=input("Enter Friends name :")
+lang=input("Enter languange name :")
+
+d.update({name:lang})
+print(d)
